023bda172755f091b7ef0faf8e587db476167cc6
antonio <antonio@ayladt.com>
2019-07-18 19:10:44 +0200

    XXXXXXXXXXXXXX

--- peliculas/lib/src/providers/peliculas_provider.dart
+++ peliculas/lib/src/providers/peliculas_provider.dart
@@ -1,19 +1,31 @@
+
+
 /**
  * 
  */
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 import 'package:peliculas/src/models/pelicula_model.dart';
 
 class PeliculasProvider {
-  String _apikey = '62b8db3ff0a4815d7a25f0589f5944c2';
+  String _apikey = 'XXXXXXXXXXXXXXXXXXXXXXXXXX';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
   String _apiNumber = '3';
 
-  Future<List<Pelicula>> _procesarRespuesta( Uri url ) async {
-    
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = new List();
+
+  final _popularesStream = StreamController<List<Pelicula>>.broadcast();
+
+  void disposeStreams() {
+    _popularesStream?.close();
+  }
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     // final url = Uri.https(_url, url, {
     //   'api_key': _apikey,
     //   'language': _language}
@@ -22,11 +34,9 @@ class PeliculasProvider {
     final decodedData = json.decode(resp.body);
     final peliculas = new Peliculas.fromJsonList(decodedData['results']);
     return peliculas.items;
-
   }
 
   Future<List<Pelicula>> getEnCines() async {
-
     String _unencodedPath = _apiNumber + '/movie/now_playing';
     final url = Uri.https(
         _url, _unencodedPath, {'api_key': _apikey, 'language': _language});
@@ -35,14 +45,15 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+    _popularesPage++;
 
     String _unencodedPath = _apiNumber + '/movie/popular';
-    final url = Uri.https(
-        _url, _unencodedPath, {'api_key': _apikey, 'language': _language});
+    final url = Uri.https(_url, _unencodedPath, {
+      'api_key': _apikey,
+      'language': _language,
+      'page': _popularesPage.toString()
+    });
 
     return await _procesarRespuesta(url);
   }
-
-
-
 }
